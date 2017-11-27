7c2ec47b10a14e1b4e95ad3c5d5c0ea9cfdba1a5
unknown <hemken@SSCTS05.ads.ssc.wisc.edu>
2017-11-27 08:19:00 -0600

    use exiting profile.do

--- R/stata_collectcode.r
+++ R/stata_collectcode.r
@@ -1,7 +1,8 @@
 stata_collectcode <- function() {
-    if (file.exists("profile.do")) {
-        stop("You must remove or rename an existing 'profile.do'.")
-    }
+  if (file.exists("profile.do")){
+    oprofile <- readLines("profile.do")
+    message("Found an existing profile.do")
+  }
 knitr::knit_hooks$set(collectcode = function(before, options, envir) {
     if (!before) {
         if (options$engine == “stata“) {
@@ -13,6 +14,9 @@ knitr::knit_hooks$set(collectcode = function(before, options, envir) {
         do.call("on.exit",
             list(quote(unlink("profile.do")), add=TRUE),
             envir = sys.frame(-9))
+        do.call("on.exit",
+                list(quote(writeLines(oprofile, "profile.do")), add=TRUE),
+                envir = sys.frame(-9))
             # sys.frame(1) or sys.frame(-10) is rmarkdown::render()
             # sys.frame(-9) is knitr::knit()
         }
