6c6056dfd1b85827455f41d8114a4082b1082947
Douglas Hemken <dehemken@wisc.edu>
2019-08-12 13:58:23 -0500

    added Stata-IC

--- R/find_stata.r
+++ R/find_stata.r
@@ -7,8 +7,8 @@ find_stata <- function(message=TRUE) {
       for (v in 11:16) {
         dv <- paste(d,paste0("Stata",v), sep="/")
         if (dir.exists(dv)) {
-          for (f in c("Stata", "StataSE", "StataMP",
-               "Stata-64", "StataSE-64", "StataMP-64")) {
+          for (f in c("Stata", "StataIC", "StataSE", "StataMP",
+               "Stata-64", "StataIC-64", "StataSE-64", "StataMP-64")) {
             dvf <- paste(paste(dv, f, sep="/"), "exe", sep=".")
             if (file.exists(dvf)) {
               stataexe <- dvf
