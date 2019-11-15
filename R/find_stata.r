92563ccda293091327bc42206e8a5b202018f362
Douglas Hemken <dehemken@wisc.edu>
2019-11-15 15:14:31 -0600

    better search for Windows Stata executable

--- R/find_stata.r
+++ R/find_stata.r
@@ -6,19 +6,21 @@ find_stata <- function(message=TRUE) {
     if (stataexe=="" & dir.exists(d)) {
       for (v in seq(16,11,-1)) {
         dv <- paste(d,paste0("Stata",v), sep="/")
-        if (stataexe=="" & dir.exists(dv)) {
+        if (dir.exists(dv)) {
           for (f in c("Stata", "StataIC", "StataSE", "StataMP",
                "Stata-64", "StataIC-64", "StataSE-64", "StataMP-64")) {
             dvf <- paste(paste(dv, f, sep="/"), "exe", sep=".")
             if (file.exists(dvf)) {
               stataexe <- dvf
               if (message) message("Stata found at ", stataexe)
-              break
             }
+            if (stataexe != "") break
           }
-        }
+       }
+       if (stataexe != "") break
       }
     }
+    if (stataexe != "") break
   }
   } else if (Sys.info()["sysname"]=="Darwin") {
 #    stataexe <- NULL
