e884ecf77b2d0f23d6d268a19bf68a818461eeb1
Tom Palmer <remlapmot@hotmail.com>
2026-09-20 16:02:55 +0100

    Amend equals to back arrow

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 # unexported from 'knitr' version 1.43 (where it is named 'one_string')
 # author: Yihui Xie
 # used in engine_output.R
-single_string = function(x, ...) paste(x, ..., collapse = '\n')
+single_string <- function(x, ...) paste(x, ..., collapse = '\n')
